55e8d32ab50ffa332e003f422247eaf9dbd3e5b7
Akkari Ramzi <rakkari@SQLI400059.SQLI>
2019-07-15 16:21:57 +0200

    correctif

--- src/main/java/com/split/Controller/SplitingController.java
+++ src/main/java/com/split/Controller/SplitingController.java
@@ -2,10 +2,13 @@ package com.split.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.xml.sax.SAXException;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
+import org.apache.tika.exception.TikaException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.split.entities.Page;
@@ -29,4 +32,16 @@ public class SplitingController {
 	     return pages;
 	        
 	    }
+	//test
+		@GetMapping(value="/test")
+		
+		 public List<Page> extract() throws InvalidPasswordException, IOException {
+
+		     List <Page>   pages = new ArrayList<Page>();
+		     pages = splitingService.Extract();
+		     
+		     return pages;
+		        
+		    }
+	
 }
--- src/main/java/com/split/Service/SplitingService.java
+++ src/main/java/com/split/Service/SplitingService.java
@@ -1,17 +1,27 @@
 package com.split.service;
 
+import org.apache.pdfbox.cos.COSInputStream;
 import org.apache.pdfbox.multipdf.Splitter;
-
+import org.xml.sax.ContentHandler;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.common.PDStream;
 import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.apache.pdfbox.text.PDFTextStripperByArea;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.pdf.PDFParser;
+import org.apache.tika.sax.BodyContentHandler;
 import org.springframework.stereotype.Service;
+import org.xml.sax.SAXException;
 
 import com.split.entities.Page;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,7 +35,7 @@ public class SplitingService {
 		List<Page> Extracted_pages = new ArrayList<Page>();
 
 		// Loading an existing PDF document
-		File file = new File("C:/Users/rakkari/Desktop/Airbus.pdf");
+		File file = new File("C:/Users/rakkari/Desktop/Total.pdf");
 
 		PDDocument document = PDDocument.load(file);
 
@@ -62,4 +72,43 @@ public class SplitingService {
 		}
 		return Extracted_pages;
 	}
+	public List<Page> Extract() throws InvalidPasswordException, IOException {
+		String pdfFileInText = "";
+		String result = " ";
+		List<Page> Extracted_pages = new ArrayList<Page>();
+
+		// Loading an existing PDF document
+		File file = new File("C:/Users/rakkari/Desktop/Total.pdf");
+
+		PDDocument document = PDDocument.load(file);
+
+		// Instantiating Splitter class
+		Splitter splitter = new Splitter();
+
+		// splitting the pages of a PDF document
+		List<PDDocument> Pages = splitter.split(document);
+
+		for (int i = 0; i < Pages.size(); i++) {
+			try (PDDocument doc = Pages.get(i)) {
+
+				doc.getClass();
+
+				if (!document.isEncrypted()) {
+
+					PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+					stripper.setSortByPosition(true);
+
+					PDFTextStripper tStripper = new PDFTextStripper();
+
+					pdfFileInText = tStripper.getText(doc).replace("\r\n", "");
+				}
+				// trim "\\r?\\n" from all the page
+				
+				// creating the page i+1 is the number of the page & result is the text
+				Page page = new Page(i + 1, pdfFileInText);
+				Extracted_pages.add(page);
+			}
+
+		}
+		return Extracted_pages;}
 }
